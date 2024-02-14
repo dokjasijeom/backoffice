27931101b540cf83a15cbabc228d17ffc20fecf8
gyeonggeun oh <gyeong9768@gmail.com>
2024-02-14 21:01:35 +0900

    chore: 기본 메타 태그 수정

--- app.vue
+++ app.vue
@@ -5,3 +5,16 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "독자시점 백오피스",
+  meta: [
+    {
+      name: "description",
+      content:
+        "독자시점 콘텐츠 관리 페이지 입니다. 인가된 사용자만 접근할 수 있습니다.",
+    },
+  ],
+});
+</script>
